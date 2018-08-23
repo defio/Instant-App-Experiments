287d3b17a6089c45b38e9117d87bd7f841bffddb
Nicola De Fiorenze <n.defiorenze@doveconviene.com>
2018-08-23 16:38:46 +0200

    landing activity

--- app/src/main/java/com/ndefiorenze/weatherinstantappexample/capitals/view/CapitalsActivity.kt
+++ app/src/main/java/com/ndefiorenze/weatherinstantappexample/capitals/view/CapitalsActivity.kt
@@ -2,6 +2,7 @@ package com.ndefiorenze.weatherinstantappexample.capitals.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.ndefiorenze.weatherinstantappexample.R
 
 class CapitalsActivity : AppCompatActivity() {
@@ -9,5 +10,19 @@ class CapitalsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.capitals_activity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        title = "European capital's Weather"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
