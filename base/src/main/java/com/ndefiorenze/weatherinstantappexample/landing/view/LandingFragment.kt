d6d94f7e2d6b31f4d99e3048fb02bedeeac5e79f
Nicola De Fiorenze <n.defiorenze@doveconviene.com>
2018-08-24 15:28:14 +0200

    replace intent with destination class with intent with url

--- base/src/main/java/com/ndefiorenze/weatherinstantappexample/landing/view/LandingFragment.kt
+++ base/src/main/java/com/ndefiorenze/weatherinstantappexample/landing/view/LandingFragment.kt
@@ -1,15 +1,15 @@
 package com.ndefiorenze.weatherinstantappexample.landing.view
 
 import android.app.Fragment
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.ndefiorenze.weather.base.R
-import com.ndefiorenze.weatherinstantappexample.capitals.view.CapitalsActivity
-import com.ndefiorenze.weatherinstantappexample.city.view.CityActivity
 import kotlinx.android.synthetic.main.landing_fragment.button_capitals
 import kotlinx.android.synthetic.main.landing_fragment.button_city
 
@@ -30,17 +30,37 @@ class LandingFragment : Fragment() {
         buttonCity = button_city
 
         buttonCapitals.setOnClickListener{
-            startActivity(Intent(this@LandingFragment.activity, CapitalsActivity::class.java))
+            val starter = baseIntent(URL_CAPITALS, activity)
+            activity.startActivity(starter)
         }
         buttonCity.setOnClickListener{
-            startActivity(Intent(this@LandingFragment.activity, CityActivity::class.java))
+            val starter = baseIntent(URL_CITY, activity)
+            activity.startActivity(starter)
         }
 
+    }
+
+    private fun baseIntent(url: String, context: Context? = null): Intent {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                .addCategory(Intent.CATEGORY_DEFAULT)
+                .addCategory(Intent.CATEGORY_BROWSABLE)
+        if (context != null) {
+            intent.`package` = context.packageName
+        }
+        return intent
     }
 
     override fun onStop() {
         super.onStop()
         buttonCapitals.setOnClickListener(null)
         buttonCity.setOnClickListener(null)
+    }
+
+
+    companion object {
+        private const val URL_BASE = "https://weather.ndefiorenze.com"
+        private const val URL_LANDING = "$URL_BASE/landing"
+        private const val URL_CAPITALS = "$URL_BASE/capitals"
+        private const val URL_CITY = "$URL_BASE/city"
     }
 }
